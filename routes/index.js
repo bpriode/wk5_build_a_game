999e3c4cf72a3b4520b3048a33ff84c547e6323d
Bethany Priode <bpriode@gmail.com>
2017-08-20 21:45:39 -0400

    guesses not taking away correctly

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+
+// let words = ["classroom", "tennessee", "atlanta", "cat"];
+let randomWord  = words[Math.floor(Math.random() * words.length)];
+let mysteryWord= [];
+
+
+router.get("/", function(req, res){
+  if (mysteryWord == "") {
+     randomWord;
+    for (var i = 0; i < randomWord.length; i++) {
+      let character =
+        {
+        character : randomWord[i],
+        placeholder: "_"
+        }
+        mysteryWord.push(character);
+    }
+  }
+    let remainingGuesses = {remainingGuesses: 8}
+    
+    mysteryWord.push(remainingGuesses);
+
+  res.render("game", {mysteryWord: mysteryWord});
+
+});
+
+
+router.post('/game', function(req, res) {
+  let guessedLetter = req.body.character;
+  if(randomWord.includes(guessedLetter)) {
+    mysteryWord.forEach(function(single) {
+      if (single.character == req.body.character) {
+        single.guessed = true;
+        res.redirect('/');
+      }
+    })
+  }else {
+    let guess = {singleGuess: req.body.character}
+    mysteryWord.push(guess)
+    res.redirect('/')
+  }
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'mustache');
 app.set('layout', 'layout');
 
+
 app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,7 +24,7 @@ app.use(morgan('dev'));
 app.use(session({
   secret:"BP",
   resave: false,
-  saveUnitialized: false;
+  saveUnitialized: false
 }));
 
 app.use(routes);
